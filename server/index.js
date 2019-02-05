6831caf9238b0ba1a6a41cc710a06428e691c69b
laughingturtle <monkeydo@Monkeys-MacBook-Pro.local>
2019-02-04 18:28:08 -0800

    setting up proxy repo

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const proxy = require('express-http-proxy');
+const app = require('express')();
+const port = process.env.PORT || 3010;
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true }));
+app.use(express.static(path.join(__dirname, '../src/client')));
+
+app.listen(port, () => {
+  console.log(`server running at: http://localhost:${port}`);
+});
