327970817f0d2a6810d40f867e2d7d2f1ab6eadb
laughingturtle <monkeydo@Monkeys-MacBook-Pro.local>
2019-03-01 12:56:33 -0800

    added http-proxy-middleware to routes

--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const proxy = require('express-http-proxy');
+//const proxy = require('express-http-proxy');
+var proxy = require('http-proxy-middleware');
 const app = require('express')();
-const port = process.env.PORT || 3010;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true }));
 app.use(express.static(path.join(__dirname, '../src/client')));
 
+const port = process.env.PORT || 3010;
+
+app.use(proxy('/users', {target: 'http://127.0.0.1:3028'}));
+app.use(proxy('/recent-broadcasts', {target: 'http://127.0.0.1:3000'}));
+app.use(proxy('/recent-highlights', {target: 'http://127.0.0.1:3000'}));
+app.use(proxy('/popular-clips', {target: 'http://127.0.0.1:3000'}));
+
 app.listen(port, () => {
-  console.log(`server running at: http://localhost:${port}`);
+  console.log(`server running at: http://127.0.0.1:${port}`);
 });
+
